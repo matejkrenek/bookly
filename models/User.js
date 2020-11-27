6086b6a540d84e907ffe0693e149ee9365f534c1
matejkrenek <mate23.krenek@gmail.com>
2020-11-27 14:28:59 +0100

    Adding email validation with validator package

--- models/User.js
+++ models/User.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, "Email is required parameter"],
+        required: [true, 'Email is required parameter'],
         unique: true,
         lowercase: true,
+        validate: [isEmail, 'This is not a valid email address'],
     },
 
     username: {
         type: String,
-        required: [true, "Usernmae is required parameter"],
+        required: [true, 'Usernmae is required parameter'],
         unique: true,
         lowercase: true,
+        minlength: [3, 'Username must be at least 3 characters long'],
     },
 
     password: {
         type: String,
-        required: [true, "Password is required parameter"],
-        minlength: [6, "Password must be at least 6 characters long"],
+        required: [true, 'Password is required parameter'],
+        minlength: [6, 'Password must be at least 6 characters long'],
     },
 });
 
